44d66803f3a5867fd32aaf78be0e95822d3f0f3c
digpatel123 <59032434+digpatel123@users.noreply.github.com>
2023-09-07 15:26:46 +0100

    Sent motivation quote as an email from a txt file.

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+import random
 import smtplib
+import datetime as dt
 
-my_gmail = "digpatel123@myyahoo.com"
-password = ""
-with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
-    connection.starttls()
-    connection.login(user=my_gmail, password=password)
-    connection.sendmail(from_addr=my_gmail, to_addrs="digp1993@gmail.com", msg="Subject:Hello\n\nThis is the email body.")
+my_gmail = "digp1993@gmail.com"
+password = "wdnjpkrhpwhbdruw"
+
+now = dt.datetime.now()
+day_of_week = now.weekday()
+
+if day_of_week == 3:
+    with open("quotes.txt") as quotes_file:
+        all_quotes = quotes_file.readlines()
+        quote = random.choice(all_quotes)
+
+        print(quote)
+    with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+        connection.starttls()
+        connection.login(user=my_gmail, password=password)
+        connection.sendmail(from_addr=my_gmail, to_addrs="digpatel123@myyahoo.com",
+                            msg=f"Subject:Motivation\n\n{quote}")
+
