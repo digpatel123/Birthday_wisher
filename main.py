e01195aecffb57955468820762dcb6fab964510c
digpatel123 <59032434+digpatel123@users.noreply.github.com>
2023-09-11 19:07:30 +0100

    Programmed to check for the birthday from a list of people with birthdates from a csv file and send an email when there is a birthday.

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
+import csv
+import datetime
 import random
 import smtplib
-import datetime as dt
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-my_gmail = "digp1993@gmail.com"
-password = ""
 
-now = dt.datetime.now()
-day_of_week = now.weekday()
+def send_birthday_email():
+    today = datetime.datetime.now()
+    with open('birthdays.csv', 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip the header
+        for row in reader:
+            if int(row[3]) == today.month and int(row[4]) == today.day:
+                send_email(row[1], row[0])
 
-if day_of_week == 3:
-    with open("quotes.txt") as quotes_file:
-        all_quotes = quotes_file.readlines()
-        quote = random.choice(all_quotes)
 
-        print(quote)
-    with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
-        connection.starttls()
-        connection.login(user=my_gmail, password=password)
-        connection.sendmail(from_addr=my_gmail, to_addrs="digpatel123@myyahoo.com",
-                            msg=f"Subject:Motivation\n\n{quote}")
+def send_email(to_email, name):
+    random_choice = random.choice([1, 2, 3])
+    filename = f'letter_templates/letter_{random_choice}.txt'
 
+    with open(filename, 'r') as template_file:
+        template = template_file.read()
+    template = template.replace('[NAME]', name)
+    msg = MIMEMultipart()
+    msg['From'] = 'digp1993@gmail.com'
+    msg['To'] = to_email
+    msg['Subject'] = 'Happy Birthday!'
+    msg.attach(MIMEText(template, 'plain'))
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(msg['From'], 'password_here')
+    server.send_message(msg)
+    server.quit()
+
+
+if __name__ == "__main__":
+    send_birthday_email()
