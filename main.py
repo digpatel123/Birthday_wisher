1191fa81f6446cf9eacd806606558e271f1d6cd4
digpatel123 <59032434+digpatel123@users.noreply.github.com>
2023-09-13 11:33:39 +0100

    Added Exception handling.

--- main.py
+++ main.py
@@ -7,32 +7,38 @@ from email.mime.text import MIMEText
 
 
 def send_birthday_email():
-    today = datetime.datetime.now()
-    with open('birthdays.csv', 'r') as file:
-        reader = csv.reader(file)
-        next(reader)  # Skip the header
-        for row in reader:
-            if int(row[3]) == today.month and int(row[4]) == today.day:
-                send_email(row[1], row[0])
+    try:
+        today = datetime.datetime.now()
+        with open('birthdays.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)  # Skip the header
+            for row in reader:
+                if int(row[3]) == today.month and int(row[4]) == today.day:
+                    send_email(row[1], row[0])
+    except Exception as e:
+        print(f"An error occurred while sending birthday emails: {str(e)}")
 
 
 def send_email(to_email, name):
-    random_choice = random.choice([1, 2, 3])
-    filename = f'letter_templates/letter_{random_choice}.txt'
+    try:
+        random_choice = random.choice([1, 2, 3])
+        filename = f'letter_templates/letter_{random_choice}.txt'
 
-    with open(filename, 'r') as template_file:
-        template = template_file.read()
-    template = template.replace('[NAME]', name)
-    msg = MIMEMultipart()
-    msg['From'] = 'digp1993@gmail.com'
-    msg['To'] = to_email
-    msg['Subject'] = 'Happy Birthday!'
-    msg.attach(MIMEText(template, 'plain'))
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(msg['From'], 'password_here')
-    server.send_message(msg)
-    server.quit()
+        with open(filename, 'r') as template_file:
+            template = template_file.read()
+        template = template.replace('[NAME]', name)
+        msg = MIMEMultipart()
+        msg['From'] = 'digp1993@gmail.com'
+        msg['To'] = to_email
+        msg['Subject'] = 'Happy Birthday!'
+        msg.attach(MIMEText(template, 'plain'))
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(msg['From'], 'your_password')
+        server.send_message(msg)
+        server.quit()
+    except Exception as e:
+        print(f"An error occurred while sending email to {to_email}: {str(e)}")
 
 
 if __name__ == "__main__":
